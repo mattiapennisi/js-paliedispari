9c4e9bca88f286cf3476d87a49f2a9c5745c5e2a
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-14 15:14:00 +0100

    add pariedispari.js script

--- pariedispari/script.js
+++ pariedispari/script.js
@@ -1,4 +1,20 @@
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-const userChoice = 
+// L'utente sceglie pari o dispari
+const userChoice = prompt('Enter "even" or "odd"')
+console.log(userChoice);
+
+
+// L'utente sceglie un numero da 1 a 5
+const userNum = prompt('Enter a number from 1 to 5')
+console.log(userNum);
+
+
+// Funzione per generare un numero casuale da 1 a 5 per la scelta del pc
+function generatePcNum () {
+    const pcNum = Math.floor(Math.random()*5 + 1)
+    console.log(pcNum);
+} 
+
+generatePcNum()
